ddf48ccd2066061a3f94ae54621a4bc700e9708d
PhantomBlade18 <73357427+PhantomBlade18@users.noreply.github.com>
2020-11-09 20:27:32 +0000

    Made as many preparations as possible
    blah. PLease dont break anything

--- DailyNews/news/models.py
+++ DailyNews/news/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length = 50)
+   #member_set is a resulting queryset from the many to many relationship defined in member. Not necessarily needed for now
+    # articles is now a set of news articles that have a relationship with a specific category
+    def __str__(self):
+        x = "Name: "+self.name
+        return x
+
+class Member(User): #This is the user, contains the username and password fields by default
+    #Member inherits the email field from USER so not needed. call it using member.email
+    DOB = models.DateTimeField()
+    favouriteCats = models.ManyToManyField(Category)
+
+class Article(models.Model):
+    title = models.CharField(max_length = 150)
+    body = models.CharField(max_length= 1500)  #Alternatively models.TextField(max_length = 3000)
+    pub_Date = models.DateTimeField()
+    category = models.ForeignKey( to=Category,related_name='articles',on_delete=models.CASCADE)
+
+    def __str__(self):
+        x = "Title: "+self.title + " Body: "+ self.body+ " Published: "+ str(self.pub_Date)+ " Category: " +self.category.name + "\n"
+        return x
